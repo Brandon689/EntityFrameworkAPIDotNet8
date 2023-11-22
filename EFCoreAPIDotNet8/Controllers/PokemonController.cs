466d94d25a4c797a625ef3ba2edd4c399d8a6cd9
Brandon <brandon139@protonmail.com>
2023-11-22 18:33:07 +1100

    CRUD

--- EFCoreAPIDotNet8/Controllers/PokemonController.cs
+++ EFCoreAPIDotNet8/Controllers/PokemonController.cs
@@ -24,5 +24,37 @@ namespace EntityFrameworkAPIDotNet8.Controllers
             var pokemon = await _context.Pokemon.ToListAsync();
             return Ok(pokemon);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pokemon>> GetPokemon(int id)
+        {
+            var pokemon = await _context.Pokemon.FindAsync(id);
+            if (pokemon is null)
+                return NotFound("Pokemon not found.");
+            return Ok(pokemon);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Pokemon>>> AddPokemon(Pokemon pokemon)
+        {
+            _context.Pokemon.Add(pokemon);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Pokemon.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Pokemon>>> UpdatePokemon(Pokemon updatedPokemon)
+        {
+            var dbPokemon = await _context.Pokemon.FindAsync(updatedPokemon.Id);
+            if (dbPokemon is null)
+                return NotFound("Pokemon not found.");
+            dbPokemon.Name = updatedPokemon.Name;
+            dbPokemon.Hp = updatedPokemon.Hp;
+            dbPokemon.Description = updatedPokemon.Description;
+            dbPokemon.Type = updatedPokemon.Type;
+
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Pokemon.ToListAsync());
+        }
     }
 }
